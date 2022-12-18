492e4d99e4c731758029907098103f6ae19b4e23
diyorbeknurillayev <112867809+diyorbeknurillayev@users.noreply.github.com>
2022-12-18 19:59:58 +0300

    UPdate Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,7 +7,7 @@ type Props = {}
 
 export default function Hero({ }: Props) {
     const [text, count] = useTypewriter({
-        words: ["Hi, The Name's Sonny Sangha", "Guy-who-loves-Coffe.tsx", "<ButLovesToCodeMore />"],
+        words: ["Hi, The Name's Diyorbek Nurullayev", "Guy-who-loves-Coffe.tsx", "<ButLovesToCodeMore />"],
         loop: true,
         delaySpeed: 2000,
 
@@ -18,7 +18,7 @@ export default function Hero({ }: Props) {
             <img className='relative rounded-full h-32 w-32 mx-auto object-cover' src="https://cdn.sanity.io/images/ltuexkre/production/a766c574c295f2103c63c2dfdf675557c664d016-600x600.png" alt="" />
             <div className='z-20'>
                 <h2 className='text-sm uppercase text-gray-500 pb-2 tracking-[15px]'>Software Engineer</h2>
-                <h1 className='text-5xl lg:text-6xl font-semibold scroll-px-10'>
+                <h1 className='text-5xl font-semibold scroll-px-10'>
                     <span className='mr-2'>{text}</span>
                     <Cursor cursorColor='#f7ab0a' />
                 </h1>
